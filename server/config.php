03e0ea6e70435de5f469070947b6423bc410f9a3
LapoBiso <lapo.bisognin@stud.unifi.it>
2022-07-15 18:12:24 +0200

    FUNZIONA

--- server/config.php
+++ server/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:8889');
 define('DB_USER', 'ProvaServer');
 define('DB_PASSWORD', 'ProvaServer');
 define('DB_DATABASE', 'provaserver');
